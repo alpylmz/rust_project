fd785b76c1e5a58b5c06b8874af2094cb7b7bfcc
alpylmz <>
2024-12-11 00:06:45 -0500

    deleting unnecessary code

--- src/helper.rs
+++ src/helper.rs
@@ -1,4 +1,3 @@
-use std::sync::Mutex;
 
 // a struct that contains an enum of the variable(whether it is a scalar, vector or matrix) and,
 // if it is a scalar, only one lower and upper bound
@@ -16,7 +15,7 @@ pub enum Input<'a> {
     Matrix(&'a str, Vec<Vec<Bounds>>),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum VarType {
     Scalar,
     Vector,
@@ -24,23 +23,6 @@ pub enum VarType {
 }
 
 
-// have a struct to hold all the variables, because we may need some checks on them, and keeping track of everything will be easier
-// they should also contain the vartype, and maybe some other information that we may need
-// because of that, I will just put the info on a struct so that I can easily expand it
-
-pub struct VarInfo {
-    pub name: String,
-    pub vartype: VarType
-}
-
-lazy_static::lazy_static! {
-    static ref all_variables: Mutex<Vec<VarInfo>> = Mutex::new(Vec::new());
-}
-
-pub fn add_variable(info: VarInfo) {
-    let mut variables = all_variables.lock().unwrap();
-    variables.push(info);
-}
 
 
 
